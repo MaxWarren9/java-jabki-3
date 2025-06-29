2af776a6821b2756f7b3e994828dc0894f7b732e
MaxWarren9 <maximvoinoff@gmail.com>
2025-06-29 17:10:57 +0300

    Добавлено домашнее задание 3 с тестами

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         printHello();
@@ -8,6 +12,37 @@ public class Main {
         System.out.printf("%s * %s * %s = %s%s", 2, 3, 4, multiply(2, 3, 4), System.lineSeparator());
 
         System.out.printf("%s! = %s%s", 3, factorial(3), System.lineSeparator());
+
+//        Домашнее задание. 1.
+        int a = 5;
+        int b = 9;
+        System.out.printf("Сумма чисел между %d и %d включительно равна " + sumRange(a, b), a, b);
+        System.out.println();
+
+//        2.
+
+        String greeting = "Привет, друг! \n";
+        String feeling = "Как ты себя чувствуешь?";
+        System.out.println(add(greeting, feeling));
+        System.out.println(add(a, b));
+
+//        3.
+        int[] list = new int[]{1, 2, 52, 4, 32, -5};
+        System.out.println(findMax(list));
+
+//        4.
+        System.out.println(calculateFactorial(3));
+
+//        5.
+        System.out.println(calculateArea(5));
+
+        System.out.println(calculateArea(3, 4));
+
+//        6.
+        System.out.println(calculateAverage(2, 5, 9));
+
+//        7.
+        System.out.println(calculateHypotenuse(15.0, 8.0));
     }
 
     public static void printHello() {
@@ -30,6 +65,7 @@ public class Main {
         return a * b * c;
     }
 
+
     /**
      * 3! = 3 * 2 * 1 = 6
      * 3! = 3 * 2! = 6
@@ -51,4 +87,116 @@ public class Main {
         }
         return n * factorial(n - 1);
     }
+
+
+    /*
+    Домашнее задание.
+    1. Создайте метод sumRange, который принимает два числа
+    (начало и конец диапазона) и возвращает сумму всех чисел между ними (включительно).
+    2. Создайте два перегруженных метода add:
+        Первый складывает два целых числа;
+        Второй складывает две строки.
+    3. Напишите метод findMax, который принимает массив целых чисел
+        и возвращает его максимальный элемент.
+    4. Напишите метод, вычисляющий факториал числа с помощью цикла for
+    5. Создайте перегруженные методы calculateArea:
+        Для круга (принимает радиус);
+        Для прямоугольника (принимает длину и ширину).
+    6. Реализуйте метод, который принимает три числа и возвращает их среднее арифметическое.
+    7. Напишите метод, который принимает два числа, представляющие длины катетов,
+        и возвращает длину гипотенузы (используйте теорему Пифагора).
+     */
+
+    // Метод 1. сумма чисел
+
+    public static int sumRange(int a, int b) {
+        if (a > b) {
+            throw new IllegalArgumentException("Первое число в диапазоне должно быть меньше второго числа. Введите корректные параметры для чисел.");
+        }
+            int sum = 0;
+            for (int i = a; i <= b; i++) {
+                sum += i;
+            }
+            return sum;
+    }
+
+//    Метод 2. Сумма чисел и конкатенация
+
+    public static int add(int a, int b) {
+        long result = (long) a + b;
+        if (result > Integer.MAX_VALUE) {
+            throw new ArithmeticException("Сумма чисел больше максимально допустимой.");
+        }
+        return (int) result;
+    }
+
+    public static String add(String one, String two) {
+        return one + two;
+    }
+
+//    Метод 3. Максимальное значение массива
+
+    public static int findMax(int[] numbers) {
+        if (numbers.length == 0 || numbers == null) {
+            throw new IllegalArgumentException("Данный метод не работает с пустым массивом.");
+        }
+        int biggestNumber = numbers[0];
+        for (int num : numbers) {
+            if (num > biggestNumber) {
+                biggestNumber = num;
+            }
+        }
+        return biggestNumber;
+    }
+
+//    Метод 4. Вычисление факториала числа через цикл for
+
+    public static int calculateFactorial(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Факториал может быть посчитан только для натуральных чисел.");
+        }
+        int factorial = 1;
+        for (int i = number; i > 0; i--) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+
+//    Метод 5. Расчет площади круга и прямоугольника (с точностью до двух знаков для круга)
+
+    public static double calculateArea(int radius) {
+        if (radius < 0) {
+            throw new IllegalArgumentException("Введите положительное значение радиуса.");
+        }
+        DecimalFormatSymbols otherSymbols = new DecimalFormatSymbols(Locale.getDefault());
+        otherSymbols.setDecimalSeparator('.');
+        String pattern = "#.##";
+        DecimalFormat decimalFormat = new DecimalFormat(pattern, otherSymbols);
+        String result = decimalFormat.format(Math.PI * Math.pow(radius, 2));
+        return Double.parseDouble(result);
+    }
+
+    public static int calculateArea(int length, int width) {
+        if (length < 0 || width < 0) {
+            throw new IllegalArgumentException("Введите положительные значения сторон.");
+        }
+        return length * width;
+    }
+
+//    Метод 6. Расчет среднего арифметического для трех чисел
+
+    public static double calculateAverage(double firstNumber, double secondNumber, double thirdNumber) {
+        return (firstNumber + secondNumber + thirdNumber) / 3;
+    }
+
+//    Метод 7. Расчет длины гипотенузы по двум катетам
+
+    public static double calculateHypotenuse(double cathetus1, double cathetus2) {
+        if (cathetus1 <= 0 || cathetus2 <= 0) {
+            throw new IllegalArgumentException("Катет не может быть меньше или равен 0");
+        }
+        double poweredHypotenuse = Math.pow(cathetus1, 2) + Math.pow(cathetus2, 2);
+        return Math.sqrt(poweredHypotenuse);
+    }
 }
+
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,6 +1,10 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.EmptyStackException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class MainTest {
     @Test
     public void hello() {
@@ -14,4 +18,114 @@ public class MainTest {
         int b = 3;
         Assertions.assertEquals(a * b, Main.multiply(a, b));
     }
+
+    @Test
+    public void sumRange_IntegerNumbers_ReturnsCorrectSum() {
+        Assertions.assertEquals(9, Main.sumRange(2, 4));
+        Assertions.assertEquals(0, Main.sumRange(0, 0));
+        Assertions.assertEquals(3, Main.sumRange(3, 3));
+        Assertions.assertEquals(0, Main.sumRange(-3, 3));
+    }
+
+    @Test
+    public void sumRange_FirstNumberIsBiggerThanSecondNumber_ThrowsException() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> Main.sumRange(3, 2));
+    }
+
+    @Test
+    public void add_IntegerNumbers_ReturnsCorrectSum() {
+        Assertions.assertEquals(3, Main.add(1, 2));
+        Assertions.assertEquals(0, Main.add(500, -500));
+        Assertions.assertEquals(0, Main.add(0, 0));
+        Assertions.assertEquals(-5, Main.add(-2, -3));
+    }
+
+    @Test
+    public void add_TwoMaxIntegers_ReturnsException() {
+        assertThrows(ArithmeticException.class, () -> Main.add(2147483647, 2147483647));
+    }
+
+    @Test
+    public void add_Strings_ReturnsConcatenatedStrings() {
+        Assertions.assertEquals("Hello", Main.add("He", "llo"));
+        Assertions.assertEquals("Hello1", Main.add("Hello", "1"));
+        Assertions.assertEquals("Hello", Main.add("", "Hello"));
+    }
+
+    @Test
+    public void findMax_ArrayOfIntegers_ReturnsCorrectMaxValue() {
+
+        Assertions.assertEquals(5, Main.findMax(new int[]{1, 2, 3, 4, 5}));
+        Assertions.assertEquals(0, Main.findMax(new int[]{-1, -2, -3, 0, -4}));
+        Assertions.assertEquals(-1, Main.findMax(new int[]{-1, -2, -3, -4}));
+    }
+
+    @Test
+    public void findMax_EmptyArray_ShouldThrowException() {
+        assertThrows(IllegalArgumentException.class, () -> Main.findMax(new int[]{}));
+    }
+
+    @Test
+    public void calculateFactorial_NaturalNumber_ReturnsCorrectFactorial() {
+        Assertions.assertEquals(120, Main.calculateFactorial(5));
+        Assertions.assertEquals(1, Main.calculateFactorial(1));
+        Assertions.assertEquals(1, Main.calculateFactorial(0));
+    }
+
+    @Test
+    public void calculateFactorial_NegativeNumber_ShouldThrowException() {
+        assertThrows(IllegalArgumentException.class, () -> Main.calculateFactorial(-5));
+    }
+
+    @Test
+    public void calculateArea_RadiusCorrectValue_ReturnsCorrectSquare() {
+        Assertions.assertEquals(78.54, Main.calculateArea(5));
+        Assertions.assertEquals(3.14, Main.calculateArea(1));
+        Assertions.assertEquals(0, Main.calculateArea(0));
+    }
+
+    @Test
+    public void calculateArea_LengthAndWidthCorrectValue_ReturnsCorrectSquare() {
+        Assertions.assertEquals(12, Main.calculateArea(3, 4));
+        Assertions.assertEquals(3, Main.calculateArea(3, 1));
+        Assertions.assertEquals(0, Main.calculateArea(0, 4));
+    }
+
+    @Test
+    public void calculateArea_RadiusIncorrectValue_ShouldThrowException() {
+        assertThrows(IllegalArgumentException.class, () -> Main.calculateArea(-1));
+    }
+
+    @Test
+    public void calculateArea_LengthOrWidthIncorrectValue_ShouldThrowException() {
+        assertThrows(IllegalArgumentException.class, () -> Main.calculateArea(-1, 3));
+        assertThrows(IllegalArgumentException.class, () -> Main.calculateArea(1, -3));
+        assertThrows(IllegalArgumentException.class, () -> Main.calculateArea(-1, -3));
+    }
+
+    @Test
+    public void calculateAverage_CorrectValues_ReturnsCorrectAverage() {
+        Assertions.assertEquals(4, Main.calculateAverage(3, 4, 5));
+        Assertions.assertEquals(2.66, Main.calculateAverage(4, 0, 4), 0.01);
+        Assertions.assertEquals(-4, Main.calculateAverage(-4, -4, -4));
+        Assertions.assertEquals(0, Main.calculateAverage(0, 0, 0));
+        Assertions.assertEquals(0.33, Main.calculateAverage(0, 0.33, 0.66), 0.01);
+    }
+
+    @Test
+    public void calculateHypotenuse_CorrectValues_ReturnsCorrectHypotenuse() {
+        Assertions.assertEquals(5, Main.calculateHypotenuse(3, 4), 0);
+        Assertions.assertEquals(35.35, Main.calculateHypotenuse(25, 25), 0.01);
+        Assertions.assertEquals(2.23, Main.calculateHypotenuse(1, 2), 0.01);
+    }
+
+    @Test
+    public void calculateHypotenuse_OneOrBothCathetiAreLessOrEqualToZero_ShouldThrowException() {
+        assertThrows(IllegalArgumentException.class, () -> Main.calculateHypotenuse(3, 0));
+        assertThrows(IllegalArgumentException.class, () -> Main.calculateHypotenuse(0, 3));
+        assertThrows(IllegalArgumentException.class, () -> Main.calculateHypotenuse(0, 0));
+        assertThrows(IllegalArgumentException.class, () -> Main.calculateHypotenuse(0, -3));
+        assertThrows(IllegalArgumentException.class, () -> Main.calculateHypotenuse(-1, -3));
+    }
+
 }
